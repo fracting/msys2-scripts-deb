d1536f58dc7ae8b09dce24044fdc0993544f5e43
Qian Hong <qhong@codeweavers.com>
2015-08-11 04:41:33 +0800

    Fix msys2 package name.

--- upload.sh
+++ upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 expect -c "
-  spawn scp *_v1.tar.gz msys2@106.186.28.242:~/Dropbox/Public/msys2/
+  spawn scp ~/msys2-pack*.tar.gz msys2@106.186.28.242:~/Dropbox/Public/msys2/
   expect {
     \"*assword\" {set timeout 300; send \"msys2\r\";}
     \"yes/no\" {send \"yes\r\"; exp_continue;}
