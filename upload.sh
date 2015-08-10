decd11bf41541c535f2c74ea40791edd9edff7b6
Qian Hong <qhong@codeweavers.com>
2015-08-11 05:07:50 +0800

    Fix upload.

--- upload.sh
+++ upload.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+NEW_MSYS2_PACKAGE=`ls ~/msys2-pack*.tar.gz`
 expect -c "
-  spawn scp ~/msys2-pack*.tar.gz msys2@106.186.28.242:~/Dropbox/Public/msys2/
+  spawn scp ${NEW_MSYS2_PACKAGE} msys2@106.186.28.242:~/Dropbox/Public/msys2/
   expect {
     \"*assword\" {set timeout 300; send \"msys2\r\";}
     \"yes/no\" {send \"yes\r\"; exp_continue;}
